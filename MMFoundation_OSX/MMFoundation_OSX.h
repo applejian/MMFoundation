153701066361fa9395eb46a0da19f55189d66146
Florian Buerger <florian@florianbuerger.com>
2016-02-08 14:47:50 +0100

    Add osx, watchOS and tvOS scheme

---
+++ MMFoundation_OSX/MMFoundation_OSX.h
@@ -0,0 +1,19 @@
+//
+//  MMFoundation_OSX.h
+//  MMFoundation_OSX
+//
+//  Created by Florian Bürger on 2/8/16.
+//  Copyright © 2016 Mobile Maultaschen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MMFoundation_OSX.
+FOUNDATION_EXPORT double MMFoundation_OSXVersionNumber;
+
+//! Project version string for MMFoundation_OSX.
+FOUNDATION_EXPORT const unsigned char MMFoundation_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMFoundation_OSX/PublicHeader.h>
+
+
---
+++ MMFoundation_tvOS/MMFoundation_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  MMFoundation_tvOS.h
+//  MMFoundation_tvOS
+//
+//  Created by Florian Bürger on 2/8/16.
+//  Copyright © 2016 Mobile Maultaschen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMFoundation_tvOS.
+FOUNDATION_EXPORT double MMFoundation_tvOSVersionNumber;
+
+//! Project version string for MMFoundation_tvOS.
+FOUNDATION_EXPORT const unsigned char MMFoundation_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMFoundation_tvOS/PublicHeader.h>
+
+
